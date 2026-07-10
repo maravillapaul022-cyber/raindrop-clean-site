7a00568d9b6a14002f989f468ef31641457ee4b5
maravillapaul022-cyber <maravillapaul022@gmail.com>
2026-07-10 13:22:51 -0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import logoAsset from "@/assets/raindrop-logo.png.asset.json";
+import logoAsset from "@assets/raindrop-logo.png";
 import { useState } from "react";
 import {
   Phone,
